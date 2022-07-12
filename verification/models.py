0a7bad842a11f740eaf956de3e9da0e13648b2ed
Sinan Soysal <sinancsoysal@gmail.com>
2022-07-12 11:43:44 +0300

    feat: models included

--- verification/models.py
+++ verification/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Verification(models.Model):
+    data = models.CharField(max_length=255)
+    uploadStatus = models.CharField(max_length=10)
+    completed = models.BooleanField(default=False)
+
+
+class ShortenedId(models.Model):
+    verificationId = models.ForeignKey(Verification, on_delete=models.SET_DEFAULT)
+    shortened = models.CharField(max_length=6)
+    used = models.IntegerField(default=0)
