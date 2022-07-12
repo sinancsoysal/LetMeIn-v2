559a2ded0f21f2bc7655223e90203cd4111157e9
Sinan Soysal <sinancsoysal@gmail.com>
2022-07-12 14:05:00 +0300

    feat: secrets are safe

--- LetMeIn/settings.py
+++ LetMeIn/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from LetMeIn import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w@4bl1+jc=b3oh+@dfu0vxev1fmo%wb2%cjqzuak%w+me0h@v&'
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,16 +74,7 @@ WSGI_APPLICATION = 'LetMeIn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
-    }
-}
+DATABASES = secrets.DATABASES
 
 
 # Password validation
