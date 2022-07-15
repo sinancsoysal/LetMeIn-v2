eda34daff4678678e8ab7970ca4638013bc3146a
Sinan Soysal <sinancsoysal@gmail.com>
2022-07-15 14:43:24 +0300

    fix: unique id

--- verification/models.py
+++ verification/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Verification(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     data = models.CharField(max_length=255, null=True)
     uploadStatus = models.CharField(max_length=10, default="AUTHORIZED")
     completed = models.BooleanField(default=False)
