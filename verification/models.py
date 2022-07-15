24449fb91be5b085346790bed180a15b10518753
Sinan Soysal <sinancsoysal@gmail.com>
2022-07-15 14:17:52 +0300

    fix: casting error

--- verification/models.py
+++ verification/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Verification(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     data = models.CharField(max_length=255, null=True)
     uploadStatus = models.CharField(max_length=10, default="AUTHORIZED")
     completed = models.BooleanField(default=False)
